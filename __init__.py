9c6476a2d8d7532dd974449312732f1f2ac1cb28
login4donald <josedonald@gmail.com>
2022-09-22 15:36:04 -0400

    Basic Model + UV Support
    Models support for Accessories, Weapons, Objects and Hair.
    Initial support for Character, Enemy and Costume.
    Initial support for File Export.

--- __init__.py
+++ __init__.py
@@ -1,249 +1,461 @@
 bl_info = {
-        "name": "BURST Renewal Model Tool v0.3-alpha",
-        "author": "bluesnowball18 + loggey",
-        "version": (0, 3),
-        "blender": (3, 2, 1),
-        "description": "Senran Kagura: BURST Renewal file editor"
+    "name": "Senran Kagura Blender Mesher (Experimental) r. 0050",
+    "author": "bluesnowball18 + loggey",
+    "version": (0, 6),
+    "blender": (3, 2, 1),
+    "description": "Senran Kagura: Estival and BURST model editor in Development."
 }
 
 import bpy
 from bpy_extras.io_utils import ImportHelper
 from bpy_extras.io_utils import ExportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 import struct
+import math
+import sys
 
 def import_cat_file(filepath):
-        file = open(filepath, "rb")
 
-        file.seek(0x400)
-        names = file.read(256).decode("ascii").split(",\r\n")
+    file = open(filepath, "rb")
 
-        del names[-1:]
+    file.seek(0x400)
+    names = file.read(256).decode("ascii").split(",\r\n")
+    del names[-1:]
 
-        # go to number section of tmd0 header
-        # file.seek(0x528)
+    names_index = 1
+    file_seek = 0x500
+    file_end = 0x00
+    hasEffectJoint = False
+    isCharacterModel = False
 
+
+    for name in names:
         magic, = struct.unpack("I", file.read(4))
+
+        # Identifiers
 
         file.seek(2, 1)
         verts_format, verts_format_2 = struct.unpack("BB", file.read(2))
 
+        ## ================= START DEBUG BLOCK =================
 
+        # Bounding Box Start?
+        file.seek(32, 1)
+        unknown1, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 1:", hex(unknown1))
 
-        file.seek(104, 1)
+        # BVH Data Start?
+        file.seek(20, 1)
+        unknown2, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 2:", hex(unknown2))
+
+        # there are two more addresses.
+
+        # BVH Data Start?
+        file.seek(20, 1)
+        unknown3, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 3:", hex(unknown3))
+
+        # Loading Start?
+        file.seek(4, 1)
+        unknown4, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 4:", hex(unknown4))
+
+        # Unknown Start?
+        file.seek(4, 1)
+        unknown5, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 5:", hex(unknown5))
+
+        # Rigging-Data Start?
+        file.seek(4, 1)
         faces_start, = struct.unpack("I", file.read(4))
 
-        #file.seek(4, 1)
-        #uvs_start, = struct.unpack("I", file.read(4))
+        # Unknown Start?
+        file.seek(4, 1)
+        unknown6, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 6:", hex(unknown6))
+        #print(" -", unknown6)
 
-        file.seek(28, 1)
+        # PolyGroup Start?
+        file.seek(12, 1)
+        unknown7, = struct.unpack("I", file.read(4))
+        #print("Unknown Start Value 7:", hex(unknown7))
+        #print(" -", unknown7)
+
+        file.seek(4, 1)
+
+        ## ================= END DEBUG BLOCK =================
+
+        ##file.seek(28, 1)
         verts_start, = struct.unpack("I", file.read(4))
-
-        #file.seek(28, 1)
-        #verts_start, = struct.unpack("I", file.read(4))
-
-
-
 
         file.seek(56, 1)
         faces_count, = struct.unpack("I", file.read(4))
 
-        #file.seek(12, 1)
-        #uvs_count, = struct.unpack("I", file.read(4))
-
         file.seek(12, 1)
         verts_count, = struct.unpack("I", file.read(4))
 
-        #file.seek(12, 1)
-        #verts_count, = struct.unpack("I", file.read(4))
-
-
-
         verts = []
         faces = []
-        # !
         uvs = []
+        uvmap = []
 
-        print()
-        print("File Name:", names[0])
-        print()
-        print("vertexs:", verts_count)
-        print("faces:", faces_count)
-        #print("uv coords:", uvs_count)
-        print()
-        # pl*, hr*, en* (except en17_*)
+        # Padding Set
+
         if verts_format == 0x9F and verts_format_2 == 0x74:
-                print("-- Player/Hair Detected.")
-                verts_padding = 28
-        # TODO: add support for wpn*
-        # elif verts_format == 0x97 and verts_format_2 == 0x74:
-        #        verts_padding = 20
-        # en17_*, Costume (except Bura)
+            # Player/Hair
+            verts_padding = 28
+
+        elif verts_format == 0x97 and verts_format_2 == 0x74:
+            # Weapon(s)
+            verts_padding = 20
+            hasEffectJoint = True
+            # en17_*, Costume (except Bura)
+
         elif verts_format == 0xBF and verts_format_2 == 0x74:
-                print("-- Costume Detected.")
-                verts_padding = 32
-        # acce_*
+            # Costume(s)
+            verts_padding = 32
+            isCharacterModel = True
+
         elif verts_format == 0x9F and verts_format_2 == 0x50:
-                print("-- Accessory Detected.")
-                verts_padding = 20
-        # obj*
+            # Accessory/ies
+            verts_padding = 20
+
         elif verts_format == 0xB7 and verts_format_2 == 0x50:
-                print("-- Object Detected.")
-                verts_padding = 16
-        # bg*
+            # Objects
+            verts_padding = 16
+
         elif verts_format == 0xB7 and verts_format_2 == 0x58:
-                print("-- Background Detected.")
-                verts_padding = 16
+            # Background/Scenes
+            verts_padding = 16
+
         else:
-                return
+            return
 
-        file.seek(0x500 + verts_start)
-        print("Vertex Start Position:", hex(file.tell()))
+        # Vertex & UV data -- Known Working
 
+        file.seek(file_seek + verts_start)
         i = 0
 
         while i < verts_count:
-                x, z, y = struct.unpack("fff", file.read(12))
+            x, z, y = struct.unpack("fff", file.read(12))
+            verts.append((x, -y, z))
 
-                # verts.append(struct.unpack("fff", file.read(12)))
-                verts.append((x, -y, z))
+            file.seek(verts_padding-4, 1)
+            u, v = struct.unpack("HH", file.read(4))
+            uvs.append(((u)/1024, float((-v)/1024)+1))
 
-                i += 1
-                file.seek(verts_padding, 1)
-                if i == verts_count:
-                    print("Vertex Final Position:", hex(file.tell()))
+            i += 1
+            if i == verts_count:
+                file_end = int(file.tell())
 
+        # Face/Triangle data -- Known Working
 
-        print("-- Vertices Completed.")
-        print()
-
-        file.seek(0x500 + faces_start)
-        print("Face Start Position:", hex(file.tell()))
-
+        file.seek(file_seek + faces_start)
         i = 0
 
         if verts_format_2 != 0x58:
-                while i < faces_count:
-                        faces.append(struct.unpack("HHH", file.read(6)))
-                        i += 1
-                        if i == faces_count:
-                                # print("Vertex Format: 0x58")
-                                print("Face Final Position:", hex(file.tell()))
+            while i < faces_count:
+                newFace = struct.unpack("HHH", file.read(6))
+                faces.append(newFace)
+
+                # Organize the UV corrdinates proper in the uvmap
+                uvmap.append(uvs[newFace[0]])
+                uvmap.append(uvs[newFace[1]])
+                uvmap.append(uvs[newFace[2]])
+
+                i += 1
         else:
-                while i < faces_count:
-                        faces.append(struct.unpack("III", file.read(12)))
-                        i += 1
-                        if i == faces_count:
-                                # print("Vertex Format: NOT 0x58")
-                                print("Face Final Position:", hex(file.tell()))
+            while i < faces_count:
+                newFace = struct.unpack("III", file.read(12))
+                faces.append(newFace)
 
-        print("-- Faces Completed.")
-        print()
+                i += 1
+            if i == faces_count:
+                print(file.tell())
 
-        ## adding mapping support?
-        #file.seek(0x500 + uvs_start)
-        #i = 0
+        # Prep for other mesh
 
-        #while i < uvs_count:
-            #x, z, y = struct.unpack("fff", file.read(12))
-            #print ("vert ", i, " : ", (x, -y, z))
-            #uvs.append((x, -y, z))
-            #i += 1
+        file.seek(file_end)
 
-        ##
+        if len(names) > 1:
+            file_seek = file.tell() + 256 - 1 - (file.tell() + 256 - 1) % 256
 
-        print("-- UV Mapping Completed.")
-        print()
+        file.seek(file_seek)
 
-        file.close()
+        ## Create seperate mesh
 
-        mesh = bpy.data.meshes.new(names[0])
+        mesh = bpy.data.meshes.new(names[names_index-1])
         mesh.from_pydata(verts, [], faces)
 
-        object = bpy.data.objects.new(names[0], mesh)
-
+        object = bpy.data.objects.new(names[names_index-1], mesh)
         bpy.context.collection.objects.link(object)
 
+        ## Simple Smooth Mesh
 
-        print("-- Applying Smooth Shading to Mesh.")
-        # Smooth out mesh
         mesh = object.data
+        mesh.auto_smooth_angle = math.radians(90)
+
         for f in mesh.polygons:
             f.use_smooth = True
 
+        ## Apply materials
 
-        print("-- Assigning New Meterial to Mesh.")
-        mat = bpy.data.materials.new(name=names[0])
+        mat = bpy.data.materials.new(name=names[names_index-1])
 
         if object.data.materials:
             object.data.materials[0] = mat
         else:
             object.data.materials.append(mat)
 
-        print("-- Attempting to understand MID data chunk, possible UV data...")
-        print("-- Still in development.")
-        #uv_layer = mesh.uv_layers.active.data
-        #uv_layer = mesh.uv_layers.new(name=names[0])
-        #for polpy in mesh.polygons:
-            #print("Polygon index: %d, length: %d" % (polpy.index, polpy.loop_total))
-            #for loop_index in range(polpy.loop_start, polpy.loop_start + polpy.loop_total):
-                #print("    Vertex: %d" % mesh.loops[loop_index].vertex_index)
-                # causes error ↓
-                #print("    UV: %r" % uv_layer[loop_index].uv)
+        #print()
+        #print("Statistics_____")
+        #print(" Vert count*:", verts_count)
+        #print(" Edges count:", len(mesh.edges))
+        #print(" Face count*:", faces_count)
+        ##print(" UVs len:", len(uvs)) #Same as Vert count
+        #print(" Mesh loops:", len(mesh.loops))
+        ##print(" Mesh polys:", len(mesh.polygons)) #Same as Face count
+        #print(" _____Finished mesh", names_index, "of", len(names))
 
-        print ("DONE")
+        # Pass on the uvmap to the mesh
+
+        newUV = mesh.uv_layers.new(name=names[names_index-1])
+
+        w = 0
+        while w < len(uvmap):
+            newUV.data[w].uv = uvmap[w]
+            w += 1
+
+        print()
+
+        ## Iterate to the next data stream
+
+        names_index += 1
+
+    file.close()
+    print("Complete.")
 
 
-#def extract_dds_png(filepath):
-    #file = open(filepath, "rb")
 
-    #file.seek(0x400)
-    #names = file.read(256).decode("ascii").split(",\r\n")
 
+# START debug_export -- METHOD HERE --↓
 
 def export_cat_file(filepath):
-        print("~~~Foobar~~~~")
+    file = open(filepath, "wb")
 
-class ESM_MT_export_cat(Operator, ExportHelper):
-        bl_idname = "export.cat"
-        bl_label = "Export CAT"
+    # Write File header
 
-        filename_ext = ".cat"
+    file.write(b'\x01\x00\x00\x00')
+    file.write(b'\x01\x00\x00\x00')
+    file.write(b'\x00\x00\x00\x00')
+    file.write(b'\x01\x00\x00\x00')
 
-        def execute(self, operator):
-                export_cat_file(self.properties.filepath)
-                return { "FINISHED FOOBAR" }
+    file.write(b'\x00\x00\x00\x00')
+    file.write(b'\x00\x00\x00\x00')
+    file.write(b'\x03\x00\x00\x00')
+    file.write(b'\x00\x00\x00\x00')
+
+    # Move to default starting location
+
+    file.seek(0x400)
+
+    names = []
+    mc = 0
+    i = 0
+
+    # Get all mesh in scene
+
+    objects = bpy.context.scene.objects
+    tmd_viable_mesh = []
+
+    for obj in objects:
+        if obj.type == 'MESH':
+            tmd_viable_mesh.append(obj)
+
+    # Return mesh count
+
+    print("Mesh Count:", len(tmd_viable_mesh))
+
+    # Write the Object names
+
+    while mc < len(tmd_viable_mesh):
+        file.write(tmd_viable_mesh[mc].name.encode('ascii'))
+        i += len(tmd_viable_mesh[mc].name)
+        file.write(b'\x2C\x0D\x0A')
+        i += len(b'\x2C\x0D\x0A')
+        mc += 1
+
+    # Fill empty place until next *00 offset
+
+    fill = 256-(i)
+    file.seek(fill, 1)
+
+    # Write basic mesh data sequentially
+
+    mc = 0
+    file_seek = 0x500
+    verts_padding = 0
+
+    while mc < len(tmd_viable_mesh):
+
+        mesh = tmd_viable_mesh[mc].data
+
+        verts = mesh.vertices
+        faces = mesh.polygons
+        uvmap = mesh.uv_layers.active.data
+        uvs = []
+
+        #magic,
+        file.write(b"tmd0")
+
+        #vert formats,
+        file.seek(2, 1)
+        file.write(struct.pack("BB", int("0x9F", 16), int("0x74", 16)))
+
+        #header filler
+        file.seek (2, 1)
+        file.write(struct.pack("BB", int("0x01", 16), int("0x02", 16)))
+        file.write(struct.pack("HH", int(40), int(65535)))
+
+        verts_padding = 20
+
+        ## Set simple & known identifiers
+
+        file.seek(100, 1)   # default: 104
+        face_seek_loc = file.tell()
+        print("Face Start Loc:", hex(face_seek_loc))
+        # temp
+
+        file.seek(28, 1)
+        vert_seek_loc = file.tell()
+        print("Vert Start Loc:", hex(vert_seek_loc))
+        # temp
+
+        file.seek(56, 1)
+        face_count = int(len(faces))
+        file.write(struct.pack("I", face_count))
+
+        file.seek(16, 1)    # default: 12
+        vert_count = int(len(verts))
+        file.write(struct.pack("I", vert_count))
+        print(len(verts))
+
+        # Write vertexes
+
+        file.seek(file_seek + tmp1)
+        i = 0
+
+        while i < face_count:
+            v3 = struct.pack("HHH", int(faces[i].vertices[0]), int(faces[i].vertices[1]), (int(faces[i].vertices[2])))
+            i += 1
+
+        i = 0
+        while i < vert_count:
+            v3 = struct.pack("fff", float(verts[i].co.x), float(verts[i].co.z), (float(verts[i].co.y)))
+
+            file.write(v3)
+            #print(v3)
+
+            file.seek(verts_padding, 1)
+            #print(uvmap[i].uv)
+
+            u = uvmap[i].uv.x * 1024
+            v = ((uvmap[i].uv.y * 1024))+1
+
+            v2 = struct.pack("HH", int(u), int(v))
+            file.write(v2)
+
+            print("Writing", v2, "UV Coord", i,"at", hex(file.tell()))
+
+            i += 1
+
+        print("Finished vertices.")
+
+        new_face_seek_loc = file.tell()
+        file.seek(face_seek_loc)
+        file.write(struct.pack("I", int(new_face_seek_loc)))
+        file.seek(new_face_seek_loc)
+
+
+        #file.seek()
+        # Prepare for the next avaliable \x00 offset
+
+        new_seek = file.tell() + 256 - 1 - (file.tell() + 256 - 1) % 256
+        file.seek(new_seek)
+
+        mc += 1
+
+    ## Write \x00 to the end
+    new_seek = file.tell() + 256 - 1 - (file.tell() + 256 - 1) % 256
+    file.write(struct.pack("B", 00))
+
+    file.close()
+
+    print("Export Debug Complete")
+
+# END debug_export -- METHOD HERE --↑
 
 class ESM_MT_import_cat(Operator, ImportHelper):
-        bl_idname = "import.cat"
-        bl_label = "Import CAT"
+    bl_idname = "import.cat"
+    bl_label = "Import CAT"
 
-        filename_ext = ".cat"
+    filename_ext = ".cat"
 
-        def execute(self, operator):
-                import_cat_file(self.properties.filepath)
-                return { "FINISHED" }
+    def execute(self, operator):
+        import_cat_file(self.properties.filepath)
+        return { "FINISHED" }
+
+# START debug_export -- development
+
+class ESM_MT_export_cat(Operator, ExportHelper):
+    bl_idname = "export.cat"
+    bl_label = "Export CAT"
+
+    filename_ext = ".cat"
+
+    type: EnumProperty(
+        name="Model Type",
+        description="The type of game model you are exporting.",
+        items=(
+            ('PL', "Player/Hair", "Models for the face or hair"),
+            ('WP', "Weapon", "Models for the weapon(s)"),
+            ('CO', "Costume", "Models for the costume"),
+            ('AC', "Accessory", "Model for the sccessory"),
+            ('OB', "Object", "Model for the objects"),
+            ('BG', "Field", "Model for the field or stage")
+        ),
+        default='PL',
+    )
+
+    def execute(self, operator):
+        export_cat_file(self.properties.filepath)
+        return { "FINISHED" }
+
+# FINISH debug_export -- development
 
 def menu_func(self, context):
-        self.layout.operator(ESM_MT_import_cat.bl_idname, text = "Senran Kagura: Burst Renewal (*.cat)")
-        # unimplemented
-        #self.layout.operator(ESM_MT_export_cat.bl_idname, text = "Senran Kagura: Burst Renewal (*.cat)")
-        # TEST
+    self.layout.operator(ESM_MT_import_cat.bl_idname, text = "Senran Kagura EV/BRN/PBS CAT (.cat)")
+
+# debug_export
+def menu_ex_func(self, context):
+    self.layout.operator(ESM_MT_export_cat.bl_idname, text = "Senran Kagura EV/BRN/PBS CAT (.cat) (non-functional)")
 
 def register():
-        bpy.utils.register_class(ESM_MT_import_cat)
-        bpy.types.TOPBAR_MT_file_import.append(menu_func)
-        # unimplemented
-        #bpy.utils.register_class(ESM_MT_export_cat)
-        #bpy.types.TOPBAR_MT_file_export.append(menu_func)
+    bpy.utils.register_class(ESM_MT_import_cat)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func)
+    # debug_export
+    bpy.utils.register_class(ESM_MT_export_cat)
+    bpy.types.TOPBAR_MT_file_export.append(menu_ex_func)
 
 def unregister():
-        bpy.utils.unregister_class(ESM_MT_import_cat)
-        bpy.types.TOPBAR_MT_file_import.remove(menu_func)
-        # unimplemented
-        #bpy.utils.unregister_class(ESM_MT_export_cat)
-        #bpy.types.TOPBAR_MT_file_export.remove(menu_func)
+    bpy.utils.unregister_class(ESM_MT_import_cat)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func)
+    # debug_export
+    bpy.utils.unregister_class(ESM_MT_export_cat)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_ex_func)
 
 if __name__ == "__main__":
-        register()
+    register()
